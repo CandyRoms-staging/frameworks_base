AOSPA/android_frameworks_base-old

SystemUI: Fix default data SIM resetting after reboot
CLO/qcom uses their own setting for default data SIM preference
in addition to the AOSP methods [1][2]. Account for the same in
data switch tile to prevent resetting default data SIM after a
reboot in some cases.

[1]: https://github.com/AOSPA/android_frameworks_opt_telephony/blob/3daf4d8614e0abbd76160dd815cac710071fd430/src/java/com/android/internal/telephony/vendor/VendorSubscriptionController.java#L432
[2]: https://github.com/AOSPA/android_packages_apps_Settings/blob/27edd7c597e65d2d08b53e429088ee0c6ea1c66f/src/com/android/settings/network/telephony/DataDefaultSubscriptionController.java#L45

Change-Id: Ida771e07ae7a1df0d9c20696f00512572fcb9259

packages/SystemUI/src/com/android/systemui/qs/tiles/DataSwitchTile.java
@@ -42,6 +42,7 @@ import java.util.List;
 import javax.inject.Inject;
 
 public class DataSwitchTile extends QSTileImpl<BooleanState> {
+    private static final String SETTING_USER_PREF_DATA_SUB = "user_preferred_data_sub";
     private final SubscriptionManager mSubscriptionManager;
     private final TelephonyManager mTelephonyManager;
 
@@ -229,6 +230,7 @@ public class DataSwitchTile extends QSTileImpl<BooleanState> {
                 mTelephonyManager.createForSubscriptionId(subId);
         telephonyManager.setDataEnabled(true);
         mSubscriptionManager.setDefaultDataSubId(subId);
+        Settings.Global.putInt(mContext.getContentResolver(), SETTING_USER_PREF_DATA_SUB, subId);
         Log.d(TAG, "Enabled subID: " + subId);
 
         List<SubscriptionInfo> subInfoList = mSubscriptionManager.getActiveSubscriptionInfoList(

CandyRoms-staging/frameworks_base

packages/SystemUI/src/com/android/systemui/qs/tiles/DataSwitchTile.java
@@ -56,6 +56,7 @@ public class DataSwitchTile extends QSTileImpl<BooleanState> {
             refreshState();
         }
     };
+    private static final String SETTING_USER_PREF_DATA_SUB = "user_preferred_data_sub";
     private final MyCallStateListener mPhoneStateListener;
     private final SubscriptionManager mSubscriptionManager;
     private final TelephonyManager mTelephonyManager;
@@ -261,6 +262,7 @@ public class DataSwitchTile extends QSTileImpl<BooleanState> {
         final int newSubID = newSubInfo.getSubscriptionId();
         mTelephonyManager.createForSubscriptionId(newSubID).setDataEnabled(true);
         mSubscriptionManager.setDefaultDataSubId(newSubID);
+        Settings.Global.putInt(mContext.getContentResolver(), SETTING_USER_PREF_DATA_SUB, newSubID);
         Log.d(TAG, "Enabled subID: " + newSubID);
 
         if (currentSubInfo.isOpportunistic()) {